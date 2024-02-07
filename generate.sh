76dd68e139c6507258aff8766a27bb48f6bffe03
Gabriel “gabldotink” <gabl@gabl.ink>
2024-02-07 15:07:40 -0700

    document generate.sh

--- generate.sh
+++ generate.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 # SPDX-License-Identifier: CC0-1.0
+
+# show us what we’re doing
 set -x
+
+# set the file path by generating the hash from $1, then
+# removing the filename marker from sha512sum’s output
 f=./files/"$(printf '%s' "$1"|sha512sum -b -|tr -d ' *-')"
 export f
+
+# does a file with this hash exist already?
+# if so, is it the same data? in this case, we’re fine
 if [ ! -f "$f" ]||[ "$(cat "$f")" = "$1" ]
 then printf '%s' "$1">"$f"
+# if we get to this point we’ve found a collision (two
+# different files with the exact same hash). we overwrite
+# the file and record it so that we can examine the history
+# with git blame
 else printf '%s' "$1">"$f"
 printf '%s\n' "$f">>./collisions.txt
 printf 'there is NO way you found a SHA-512 collision.
