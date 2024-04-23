1f0865663345673bad70c49e8f5032b35dd66258
Gabriel “gabldotink” <gabl@gabl.ink>
2024-04-23 09:59:25 -0600

    fix verify.sh

--- verify.sh
+++ verify.sh
@@ -2,4 +2,11 @@
 # SPDX-License-Identifier: CC0-1.0 OR 0BSD
 find ./files/ -type f -exec sh -c 'f="$1"
 export f
-[ "$(printf "%s" "$f")" = "$(cat "$f"|sha512sum -b -|tr -d " *-")" ]||printf "%s\n" "$f"' shell '{}' ';'
+s="$(basename "$f")"
+export s
+if [ "$(printf "%s" "$s")" = "$(cat "$f"|sha512sum -b -|tr -d " *-")" ]
+then printf " ok %s
+" "$s"
+else printf "bad %s
+" "$s"
+fi' shell '{}' ';'
