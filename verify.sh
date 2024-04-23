03e116a86cd821821f4a1af070be428352bb8e63
Gabriel “gabldotink” <gabl@gabl.ink>
2024-04-23 09:39:41 -0600

    I can’t resonably blame this on shellcheck… but it’s shellcheck’s fault anyway

--- verify.sh
+++ verify.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # SPDX-License-Identifier: CC0-1.0 OR 0BSD
-find ./files/ -type f -exec sh -c '"$(f="$1"
+find ./files/ -type f -exec sh -c 'f="$1"
 export f
 [ "$(printf "%s" "$f")" = "$(cat "$f"|sha512sum -b -|tr -d " *-")" ]||printf "%s\n" "$f"' shell '{}' ';'
