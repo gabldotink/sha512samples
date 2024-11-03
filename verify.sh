9238a8bdebb7f5d872ee620ce793584830a05e86
Gabriel “gabldotink” <gabl@gabl.ink>
2024-11-02 23:46:13 -0600

    edit verify.sh

--- verify.sh
+++ verify.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # SPDX-License-Identifier: CC0-1.0 OR 0BSD
-find ./files/ -type f -exec sh -c 'f="$1"
+find ./files -type f -exec sh -c 'f="$1"
 export f
 s="$(basename "$f")"
 export s
 if [ "$(printf "%s" "$s")" = "$(cat "$f"|sha512sum -b -|tr -d " *-")" ]
-then printf " ok %s
+then printf "✅ %s
 " "$s"
-else printf "bad %s
+else printf "❌ %s
 " "$s"
 fi' shell '{}' ';'
