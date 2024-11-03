245b959260991353df74b4504b05306316b62ce5
Gabriel “gabldotink” <gabl@gabl.ink>
2024-11-02 23:42:15 -0600

    optimize generate.sh

--- generate.sh
+++ generate.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
-# SPDX-License-Identifier: CC0-1.0 OR 0BSD
 d="$1"
 export d
-f=./files/"$(printf '%s' "$d"|sha512sum -b -|tr -d ' *-')"
+f="./files/$(printf '%s' "$d"|sha512sum -b|tr -d ' *-')"
 export f
 if [ ! -f "$f" ]||[ "$(cat "$f")" = "$d" ]
 then printf '%s' "$d">"$f"
 else printf '%s' "$d">"$f"
 printf '%s
 ' "$f">>./collisions.txt
-printf 'there is NO way you found a SHA-512 collision.
-it’s been recorded in ./collisions.txt, but dude, *what*?
-'
 fi
